55f1297a8c9a341d4edd5ba16b870c4962545ba3
Sébastien Saunier <seb@saunier.me>
2017-12-06 22:05:10 +0000

    Simpler boilerplate

--- src/index.jsx
+++ src/index.jsx
@@ -7,30 +7,22 @@ import logger from 'redux-logger';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
 
-import Home from './components/home';
-import About from './components/about';
 import '../assets/stylesheets/application.scss';
 
-// State and reducers
-const initialState = {
-  changeMe: null
-};
-
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  // key: reducer
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, initialState, middlewares)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
+        TODO
       </Switch>
     </Router>
   </Provider>,
-  document.querySelector('.container')
+  document.getElementById('root')
 );
